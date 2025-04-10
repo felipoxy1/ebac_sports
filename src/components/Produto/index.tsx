413df00c16741999bcff320e5432e0ce748c4774
felipoxy1 <luizfelipepassos2015@gmail.com>
2025-04-10 01:10:37 -0300

    Efetuando a conclusao do dever

--- src/components/Produto/index.tsx
+++ src/components/Produto/index.tsx
@@ -1,10 +1,13 @@
+import { useDispatch, useSelector } from 'react-redux'
 import { Produto as ProdutoType } from '../../App'
+import { adicionar } from '../../store/reducers/carrinho'
+import { toggleFavorito } from '../../store/reducers/favoritar'
+
 import * as S from './styles'
 
 type Props = {
   produto: ProdutoType
-  aoComprar: (produto: ProdutoType) => void
-  favoritar: (produto: ProdutoType) => void
+  favoritar: () => void
   estaNosFavoritos: boolean
 }
 
@@ -13,12 +16,15 @@ export const paraReal = (valor: number) =>
     valor
   )
 
-const ProdutoComponent = ({
-  produto,
-  aoComprar,
-  favoritar,
-  estaNosFavoritos
-}: Props) => {
+const ProdutoComponent = ({ produto }: Props) => {
+  const dispatch = useDispatch()
+
+  const favoritos = useSelector(
+    (state: { favoritos: { itens: ProdutoType[] } }) => state.favoritos.itens
+  )
+
+  const estaNosFavoritos = favoritos.some((f) => f.id === produto.id)
+
   return (
     <S.Produto>
       <S.Capa>
@@ -28,12 +34,17 @@ const ProdutoComponent = ({
       <S.Prices>
         <strong>{paraReal(produto.preco)}</strong>
       </S.Prices>
-      <S.BtnComprar onClick={() => favoritar(produto)} type="button">
+
+      <S.BtnComprar
+        onClick={() => dispatch(toggleFavorito(produto))}
+        type="button"
+      >
         {estaNosFavoritos
           ? '- Remover dos favoritos'
           : '+ Adicionar aos favoritos'}
       </S.BtnComprar>
-      <S.BtnComprar onClick={() => aoComprar(produto)} type="button">
+
+      <S.BtnComprar onClick={() => dispatch(adicionar(produto))} type="button">
         Adicionar ao carrinho
       </S.BtnComprar>
     </S.Produto>
--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,38 +1,36 @@
+import { useDispatch, useSelector } from 'react-redux'
 import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
+import { useGetProdutosQuery } from '../services/api'
 
 import * as S from './styles'
+import { toggleFavorito } from '../store/reducers/favoritar'
 
-type Props = {
-  produtos: ProdutoType[]
-  favoritos: ProdutoType[]
-  adicionarAoCarrinho: (produto: ProdutoType) => void
-  favoritar: (produto: ProdutoType) => void
-}
+const ProdutosComponent = () => {
+  const dispatch = useDispatch()
+  const { data: produtos, isLoading } = useGetProdutosQuery()
 
-const ProdutosComponent = ({
-  produtos,
-  favoritos,
-  adicionarAoCarrinho,
-  favoritar
-}: Props) => {
+  const favoritos = useSelector(
+    (state: { favoritos: { itens: ProdutoType[] } }) => state.favoritos.itens
+  )
+
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
-    const produtoId = produto.id
-    const IdsDosFavoritos = favoritos.map((f) => f.id)
+    return favoritos.some((f) => f.id === produto.id)
+  }
 
-    return IdsDosFavoritos.includes(produtoId)
+  const favoritar = (produto: ProdutoType) => {
+    dispatch(toggleFavorito(produto))
   }
 
   return (
     <>
       <S.Produtos>
-        {produtos.map((produto) => (
+        {produtos?.map((produto) => (
           <Produto
             estaNosFavoritos={produtoEstaNosFavoritos(produto)}
             key={produto.id}
             produto={produto}
-            favoritar={favoritar}
-            aoComprar={adicionarAoCarrinho}
+            favoritar={() => favoritar(produto)}
           />
         ))}
       </S.Produtos>
